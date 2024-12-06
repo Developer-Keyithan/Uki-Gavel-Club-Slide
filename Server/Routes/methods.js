b4e1ab0fc80972b7f2073149bbbebcb24b3d6beb
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2024-12-06 19:33:56 +0530

    Databased Connected

--- Server/Routes/methods.js
+++ Server/Routes/methods.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Players = require('../Models/rolePlayers');
+const WOTD = require('../Models/wordOfTheDay');
 
 router.get('/admin', async (req, res) => {
     try {
         const players = await Players.find();
+        const wordOfTheDay = await WOTD.find();
         res.status(200).json(players);
+        res.status(200).json(wordOfTheDay);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -14,8 +17,11 @@ router.get('/admin', async (req, res) => {
 router.get('/admin/:id', async (req, res) => {
     try {
         const player = await Players.findById(req.params.id);
+        const wordOfTheDay = await WOTD.findById(req.params.id);
         if (!player) return res.status(404).json({ error: 'Players not found' });
         res.status(200).json(player);
+        if (!wordOfTheDay) return res.status(404).json({ error: 'Data not found' });
+        res.status(200).json(wordOfTheDay);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
